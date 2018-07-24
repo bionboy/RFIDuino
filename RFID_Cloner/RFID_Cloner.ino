cb13ca06e8f1106c43acbfa155c38726423a0910
Luke Floden <luke.floden@gmail.com>
2018-07-24 13:01:34 -0400

    Cleaned up code and removed features that are not used

--- RFID_Cloner/RFID_Cloner.ino
+++ RFID_Cloner/RFID_Cloner.ino
@@ -4,24 +4,23 @@
 #define RST_PIN 9
 #define SS_PIN 10
 
-MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance
+MFRC522 mfrc522(SS_PIN, RST_PIN);
+MFRC522::MIFARE_Key key;
 
 void setup() {
-  Serial.begin(9600);        // Initialize serial communications with the PC
-  SPI.begin();               // Init SPI bus
-  mfrc522.PCD_Init();        // Init MFRC522 card
-  Serial.println(F("Write personal data on a MIFARE PICC "));
+  Serial.begin(9600);        
+  SPI.begin();               
+  mfrc522.PCD_Init();        
+  // Factory keys are all set to 0xFF
+  for (byte i = 0; i < 6; i++) key.keyByte[i] = 0xFF;
+  Serial.println("Ready to write data");
 }
 
-void loop() {
-
-  // Factory keys are all set to 0xFF
-  MFRC522::MIFARE_Key key;
-  for (byte i = 0; i < 6; i++) key.keyByte[i] = 0xFF;
-
+void loop()
+{
   if(!CardFound()) return;
 
-  Serial.print(F("UID:"));
+  Serial.print("UID:");
   for (byte i = 0; i < mfrc522.uid.size; i++) {
     Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
     Serial.print(mfrc522.uid.uidByte[i], HEX);
@@ -29,44 +28,42 @@ void loop() {
   
   byte buffer[18];
   byte block;
-  MFRC522::StatusCode status;
   byte len;
 
   Serial.setTimeout(20000L) ;     // wait until 20 seconds for input from serial
-  Serial.println(F("Type data, ending with #"));
+  Serial.println("Type data, ending with #");
   len = Serial.readBytesUntil('#', (char *) buffer, 16) ; // read family name from serial
   for (byte i = len; i < 16; i++) buffer[i] = ' ';     // pad with spaces
 
   block = 1;
   Serial.println(F("Authenticating..."));
-  status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, block, &key, &(mfrc522.uid));
-  if (status != MFRC522::STATUS_OK) {
+  if (mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, block, &key, &(mfrc522.uid))
+    != MFRC522::STATUS_OK)
+  {
     Serial.print(F("Failed to authenticate"));
-    Serial.println(mfrc522.GetStatusCodeName(status));
     return;
   }
   else Serial.println(F("Authentication Success!"));
 
   // Write block
-  status = mfrc522.MIFARE_Write(block, buffer, 16);
-  if (status != MFRC522::STATUS_OK) {
-    Serial.print(F("Failed to write"));
-    Serial.println(mfrc522.GetStatusCodeName(status));
+  if (mfrc522.MIFARE_Write(block, buffer, 16) != MFRC522::STATUS_OK)
+  {
+    Serial.print("Failed to write");
     return;
   }
-  else Serial.println(F("Write success!"));
-
-  Serial.println(" ");
+  else Serial.println("Write success!");
+  Serial.println();
+  
   mfrc522.PICC_HaltA(); // Halt PICC
   mfrc522.PCD_StopCrypto1();  // Stop encryption on PCD
 }
 
-void CardFound()
+bool CardFound()
 {
   // Look for and select card
   if ( ! mfrc522.PICC_IsNewCardPresent()) return false;
   if ( ! mfrc522.PICC_ReadCardSerial()) return false;
-  return true
+  return true;
 }
 
 
--- RFID_validator/RFID_validator.ino
+++ RFID_validator/RFID_validator.ino
@@ -5,52 +5,52 @@
 #define SS_PIN 10
 
 MFRC522 mfrc522(SS_PIN, RST_PIN);
+MFRC522::MIFARE_Key key;
 
 void setup()
 {
   Serial.begin(9600);
   SPI.begin();
-  mfrc522.PCD_Init();    
-  Serial.println(F("Read personal data on a MIFARE PICC:"));
+  mfrc522.PCD_Init();
+
+  // Init keys to 0xFF (Factory default)
+  for (byte i = 0; i < 6; i++) key.keyByte[i] = 0xFF;
+
+  Serial.println("Ready to read");  
 }
 
 void loop()
-{
-  MFRC522::MIFARE_Key key;
-  for (byte i = 0; i < 6; i++) key.keyByte[i] = 0xFF;
-
-  byte block;
-  byte len;
-  MFRC522::StatusCode status;
-
+{ 
   if (!CardFound()) return;
-  Serial.println(F("**Card Detected:**"));
 
-  mfrc522.PICC_DumpDetailsToSerial(&(mfrc522.uid)); //dump some details about the card
+  byte block = 1;
+  byte len;
+  
+  Serial.print("Data: ");
 
-  Serial.print(F("Data: "));
+  byte dataBuffer[18]; // = {4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4};
 
-  byte dataBuffer[18];
-  block = 1;
-
-  status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, 1, &key, &(mfrc522.uid)); //line 834
-  if (status != MFRC522::STATUS_OK) {
-    Serial.print(F("Authentication failed: "));
-    Serial.println(mfrc522.GetStatusCodeName(status));
+  if (mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, 1, &key, &(mfrc522.uid))
+    != MFRC522::STATUS_OK)
+  {
+    Serial.print("Authentication failed: ");
     return;
   }
 
-  status = mfrc522.MIFARE_Read(block, dataBuffer, &len);
-  if (status != MFRC522::STATUS_OK) {
-    Serial.print(F("Reading failed: "));
-    Serial.println(mfrc522.GetStatusCodeName(status));
+//  if (status = mfrc522.MIFARE_Write(block, dataBuffer, 16) != MFRC522::STATUS_OK)
+  if (mfrc522.MIFARE_Read(block, dataBuffer, &len) != MFRC522::STATUS_OK)
+  {
+    Serial.print("Reading failed: ");
     return;
   }
 
-  //PRINT DATA
-  for (uint8_t i = 0; i < 16; i++) {
-    Serial.write(dataBuffer[i] );
+  //PRINT DATA (Use Serial.Write to print as ASCII)
+  for (uint8_t i = 0; i < 16; i++)
+  {
+    Serial.print(dataBuffer[i]);
+    Serial.print(" ");
   }
+  Serial.println();
 
   delay(500);
 
@@ -58,11 +58,12 @@ void loop()
   mfrc522.PCD_StopCrypto1();
 }
 
-void CardFound()
+bool CardFound()
 {
   // Look for and select card
-  if ( ! mfrc522.PICC_IsNewCardPresent()) return false;
-  if ( ! mfrc522.PICC_ReadCardSerial()) return false;
-  return true
+  if (!mfrc522.PICC_IsNewCardPresent()) return false;
+  if (!mfrc522.PICC_ReadCardSerial()) return false;
+  Serial.println("**Card Detected:**");
+  return true;
 }
 
